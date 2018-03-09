a4047115ba539b6eac3bb3fff98a954aedbcfe61
Adrian Cumiskey <adrian.cumiskey@gmail.com>
2018-03-09 01:50:20 +0000

    Generalize further by replacing < 1000 word function

--- src/numbers_in_words/core.clj
+++ src/numbers_in_words/core.clj
@@ -17,36 +17,25 @@
              (str tens " "))
            (less-than-twenty-in-words units)))))
 
-(defn- less-than-one-thousand-in-words [n]
-  (let [hundreds (unchecked-divide-int n 100)
-        tens-and-units (rem n 100)
-        tens (unchecked-divide-int tens-and-units 10)]
-    (str (if (zero? hundreds)
-           ""
-           (str (less-than-twenty-in-words hundreds) " hundred"))
-         (if (zero? tens-and-units)
-           ""
-           (str (if (zero? hundreds)
-                  ""
-                  " and ")
-                (less-than-one-hundred-in-words tens-and-units))))))
-
-(defn- part-in-words [quotient-name quotient-fn divisor remainder-fn]
+(defn- part-in-words [quotient-name quotient-fn divisor remainder-fn anding-fn use-quotient]
   (fn [n]
     (let [quotient (unchecked-divide-int n divisor)
           remainder (rem n divisor)
-          left-over (unchecked-divide-int remainder (/ divisor 10))]
-      (str (quotient-fn quotient) " " quotient-name
+          leftover (unchecked-divide-int remainder (/ divisor 10))]
+      (str (if (zero? quotient)
+             ""
+             (str (quotient-fn quotient) " " quotient-name))
            (if (zero? remainder)
              ""
-             (str (if (zero? left-over)
-                    " and "
-                    " ")
+             (str (anding-fn (if use-quotient quotient leftover))
                   (remainder-fn remainder)))))))
 
 (defn in-words [n]
-  (let [less-than-one-million-in-words (part-in-words "thousand" less-than-one-thousand-in-words 1000 less-than-one-thousand-in-words)
-        less-than-one-trillion-in-words (part-in-words "million" less-than-one-thousand-in-words 1000000 less-than-one-million-in-words)]
+  (let [and-when-zero (fn [n] (if (zero? n) " and " " "))
+        and-when-not-zero (fn [n] (if (zero? n) "" " and "))
+        less-than-one-thousand-in-words (part-in-words "hundred" less-than-twenty-in-words 100 less-than-one-hundred-in-words and-when-not-zero true)
+        less-than-one-million-in-words (part-in-words "thousand" less-than-one-thousand-in-words 1000 less-than-one-thousand-in-words and-when-zero false)
+        less-than-one-trillion-in-words (part-in-words "million" less-than-one-thousand-in-words 1000000 less-than-one-million-in-words and-when-zero false)]
     (cond
       (< n 20) (less-than-twenty-in-words n)
       (< n 100) (less-than-one-hundred-in-words n)
